ffa30d2fa4c3c3062e03af4ebce356d3745a0ac7
Tejas Shinde <tejasmshinde5@gmail.com>
2025-02-16 18:03:01 +0530

    Your descriptive commit

--- src/Components/1ProductCard.jsx
+++ src/Components/1ProductCard.jsx
@@ -10,7 +10,7 @@ function Card(props){
     }
     return(
       
-        <div className="col-12 col-sm-6 col-lg-3 col-md-4 text-center my-3  ">
+        <div className="col-12 col-sm-6 col-md-4 col-lg-3  text-center my-3  ">
             <div className="p-1 container-product">
             <div className=" p-3 mx-2 ">
                 <div className="row">
